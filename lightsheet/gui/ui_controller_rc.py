4d550163692fcf100f94d71e6b6f168ac4405c6c
Frans Irgolitsch <f.irgolitsch@outlook.com>
2026-08-22 15:41:26 -0400

    refactor(02.1-02): regenerate pyuic5/pyrcc5 UI files with --from-imports
    - pyuic5 --from-imports ui_controller.ui: head no longer has the
      sys.path.append('./lightsheet/gui') path hack; tail now uses
      'from . import ui_controller_rc' (package-relative resource import),
      replacing the bare 'import ui_controller_rc' that was the last ty
      unresolved-import (D-05)
    - pyuic5 --from-imports ui_properties.ui: byte-identical no-op (no .qrc),
      header version bumped 5.15.6 -> 5.15.11 for consistency
    - pyrcc5 ui_controller.qrc: regenerated from inside lightsheet/gui/ (the
      .qrc references resources/*.png with CWD-relative paths); non-empty output
    - All three generated files import cleanly as package members

--- lightsheet/gui/ui_controller_rc.py
+++ lightsheet/gui/ui_controller_rc.py
@@ -2,7 +2,7 @@
 
 # Resource object code
 #
-# Created by: The Resource Compiler for PyQt5 (Qt v5.15.2)
+# Created by: The Resource Compiler for PyQt5 (Qt v5.15.14)
 #
 # WARNING! All changes made in this file will be lost!
 
@@ -15448,7 +15448,7 @@ qt_resource_struct_v2 = b"\
 \x00\x00\x00\x12\x00\x02\x00\x00\x00\x01\x00\x00\x00\x03\
 \x00\x00\x00\x00\x00\x00\x00\x00\
 \x00\x00\x00\x2a\x00\x00\x00\x00\x00\x01\x00\x00\x00\x00\
-\x00\x00\x01\x7e\xde\xfa\xb7\xae\
+\x00\x00\x01\x84\x39\x0b\x45\x76\
 "
 
 qt_version = [int(v) for v in QtCore.qVersion().split('.')]
